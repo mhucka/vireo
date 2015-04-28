57dd757f6a695a9cae98d5132ebb9557f5b82aec
Michael Hucka <mhucka@caltech.edu>
2015-04-27 19:15:33 -0700

    Made the log file viewer more fool-proof.

--- js/vireo.js
+++ js/vireo.js
@@ -143,12 +143,15 @@ var crc32 = function(str) {
     return (crc ^ (-1)) >>> 0;
 }
 
-
 function openLog(base, path) {
-   last_slash = base.lastIndexOf('/');
-   url = base.substring(0, last_slash + 1) + '/' + vireo_log_file;
-   window.open(url, '_blank');
-   return true;
+    last_slash = base.lastIndexOf('/');
+    url = base.substring(0, last_slash + 1) + vireo_log_file;
+    var win = window.open('');
+    $.get(url, function(data) {
+        $(win.document.body).html('<html><body><div id="log"><pre>'
+                                  + data + '</pre></div></body></html>');
+    });
+    return true;
 }
 
 
