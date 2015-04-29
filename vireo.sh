681458d1c98ade9a0698f9830c4f0a6613bd0386
Michael Hucka <mhucka@caltech.edu>
2015-04-29 08:25:30 -0700

    Fixed serious issues with quoted pathnames.
    This new version handles directory names with quotes in them.  This
    was annoyingly difficult to figure out, because the plain sh shell's
    behavior is really unobvious when it comes to variables referencing
    other variables that contain strings with embedded spaces in them.

--- vireo.sh
+++ vireo.sh
@@ -1,8 +1,13 @@
-#! /bin/sh
+#! /bin/sh -x
 #
 # @file    vireo.sh
 # @brief   Simple script to start/stop/restart vireo-server.py
 # @author  Michael Hucka <mhucka@caltech.edu>
+#
+# This script is suitable for Linux/Unix server installations to start and
+# stop the Vireo server when (e.g.) the computer reboots.  Depending on your
+# operating system and preferences, you might copy this script into
+# /etc/init.d, use it with Monit, or do something similar.
 #
 #<!---------------------------------------------------------------------------
 # This file is part of Vireo, the VIewer for REfreshed Output.
@@ -17,34 +22,43 @@
 # available at https://github.com/mhucka/vireo/LICENSE.txt.
 #------------------------------------------------------------------------- -->
 
+# Change the following variables to correspond to your environment.
+
+HOME=/your/home/dir
+export HOME
+
+VIREO_DIR="/change/this/path"
+VIREO_PORT=9999
+VIREO_CMD="make"
+VIREO_LOG="vireo.log"
+VIREO_SERVER="$VIREO_DIR/vireo-server.py"
+
+
+# .............................................................................
+# The rest below is generic and probably does not need to be changed.
+
 if [ `id -u` = 0 ]; then
     echo "Do not run this as root; run it as the owner of your document files."
 fi
 
-HOME=/home/mhucka
-export HOME
-
-VIREO_DIR=/home/mhucka/vireo/whole-cell-meeting-report
-VIREO_CMD="$VIREO_DIR/vireo-server.py -p 9005 -o -c make -l vireo.log"
-
 RETVAL=0
 
 case "$1" in
-  start)
-        $VIREO_CMD
+    start)
+        "$VIREO_SERVER" -d "$VIREO_DIR" -c "$VIREO_CMD" -l "$VIREO_LOG" -p $VIREO_PORT -o
         RETVAL=$?
         ;;
-  stop)
-        killall vireo-server.py
+    stop)
+        pkill --signal INT -f "$VIREO_SERVER"
         RETVAL=$?
         ;;
-  restart)
+    restart)
         $0 stop
         sleep 5
         $0 start
         RETVAL=$?
         ;;
-  *)
+    *)
         echo "Usage: $0 {start|stop|restart}" >&2
         exit 3
         ;;
